ff8d7d17e0cc1142dfda8f7e5e56b7f7d769af49
MADISONMK\joseantonio.sanchez <joxirules@gmail.com>
2018-08-20 08:36:14 +0200

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,12 @@
 import { Component } from '@angular/core';
-import { IProduct } from './products';
-import { ProductService } from './products.service';
-import { Http , Response } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 
 @Component ({
    selector: 'my-app',
-   template: '<div>Hello</div>',
-   providers: [ProductService]
+   template: `
+   <ul>
+      <li><a [routerLink] = "['/Product']">Product</a></li>
+      <li><a [routerLink] = "['/Inventory']">Inventory</a></li>
+   </ul>
+   <router-outlet></router-outlet>`
 })
-
-export   class   AppComponent  {
-   iproducts: IProduct[];
-   constructor(private _product: ProductService) {
-   }
-   
-   ngOnInit() : void {
-      this._product.getproducts()
-      .subscribe(iproducts => this.iproducts = iproducts);
-   }
-}
+export class AppComponent  { }
